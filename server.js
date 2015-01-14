f4ecc93719b5c9489bbe65b3baa085fbdbf2edc1
diego.casanova <diego.andres.casanova@gmail.com>
2015-01-14 18:05:31 -0300

    chapter 2

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ var connect = require('connect');
 
 var app = connect();
 
+var logger = function(req, res, next){
+	console.log(req.method, req.url);
+	next();
+};
 
 var helloWorld = function(req, res, next){
 	res.setHeader('Content-Type','text/plain');
 	res.end('Hello World');
 };
 
-app.use(helloWorld);
+var goodbyeWorld = function(req, res, next){
+	res.setHeader('Content-Type','text-plain');
+	res.end('Goodbye World');
+};
+
+app.use(logger);
+app.use('/hello',helloWorld);
+app.use('/goodbye',goodbyeWorld);
 
 app.listen(3000);
 
