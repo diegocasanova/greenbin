24553b9a6712e5a7f5ad804bea2fdeaddba17fdb
diego.casanova <diego.andres.casanova@gmail.com>
2015-01-15 17:33:51 -0300

    express first app

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-var connect = require('connect');
+var express = require('express');
 
-var app = connect();
+var app = express();
 
-var logger = function(req, res, next){
-	console.log(req.method, req.url);
-	next();
-};
-
-var helloWorld = function(req, res, next){
-	res.setHeader('Content-Type','text/plain');
-	res.end('Hello World');
-};
-
-var goodbyeWorld = function(req, res, next){
-	res.setHeader('Content-Type','text-plain');
-	res.end('Goodbye World');
-};
-
-app.use(logger);
-app.use('/hello',helloWorld);
-app.use('/goodbye',goodbyeWorld);
+app.use('/', function(req, res){
+	res.send('Hello World');
+});
 
 app.listen(3000);
 
+console.log('Server running at http://localhost:3000/');
 
-console.log('Server running at http://localhost:3000');
+module.exports = app;
