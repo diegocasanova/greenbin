313daefe5bf4f3cae07f723bca6c9247718bae8c
diego.casanova <diego.andres.casanova@gmail.com>
2015-05-13 23:00:42 -0300

    angular services

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,5 +1,5 @@
 angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
     function($scope, Authentication) {
-        $scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
+        $scope.authentication = Authentication;
     }
 ]);
